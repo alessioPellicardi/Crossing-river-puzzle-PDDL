91647c97afe2b820003b58a7875e05be167ba5c6
Alessio <a.pellicardi@gmail.com>
2023-05-26 11:40:03 +0200

    added 2 couple graphic

--- LPC_PDDL_GRAFICA/grafica2Couples.py
+++ LPC_PDDL_GRAFICA/grafica2Couples.py
@@ -1,4 +1,5 @@
 import subprocess
+import keyboard
 import cv2
 
 from PIL import Image, ImageTk
@@ -12,6 +13,7 @@ print(var_lettura)
 
 numeric_array = []
 image_array = []
+cont = 0
 mapping = {
     'move-wives-to-dx': 1,
     'move-wives-to-sx': 2,
@@ -31,38 +33,54 @@ for string in var_lettura:
     parts = string.split(' ')
     command = parts[0]
     numeric_array.append(mapping[command])
-    image_name = command + ".png"
-    image_array.append(image_name)
+
+#Aggiunta del 2 alla seconda versione delle mosse
+    cont = cont + 1
+    print(cont)
+    if (cont == 4):
+        image_name = command + "-2.png"
+        image_array.append(image_name)
+    elif(cont == 5):
+        image_name = command + "-2.png"
+        image_array.append(image_name)
+    else:
+        image_name = command + ".png"
+        image_array.append(image_name)
+
+
 
 print(numeric_array)
+#Inserisco manualmente immagine iniziale e finale
+image_array.insert(0, "startingPoint2Couples.png")
+image_array.insert(6,"final-situation.png")
 print(image_array)
-#
-#
-# root = tk.Tk()
-#
-# # Crea un Label per visualizzare le immagini
-# image_label = tk.Label(root)
-# image_label.pack()
-#
-# # Crea una lista per mantenere i riferimenti alle istanze di PhotoImage
-# photo_array = []
-#
-# def show_image(index):
-#     # Carica l'immagine corrispondente all'indice
-#     photo = tk.PhotoImage(file=image_array[index])
-#     photo_array.append(photo)  # Aggiunge il riferimento all'array
-#
-#     image_label.config(image=photo)
-#
-#     # Se non siamo all'ultima immagine, aspetta 750ms e mostra la prossima immagine
-#     if index < len(image_array) - 1:
-#         root.after(750, show_image, index + 1)
-#
-# # Mostra la prima immagine
-# show_image(0)
-#
-# root.mainloop()
-#
+
+
+root = tk.Tk()
+
+# Crea un Label per visualizzare le immagini
+image_label = tk.Label(root)
+image_label.pack()
+
+# Crea una lista per mantenere i riferimenti alle istanze di PhotoImage
+photo_array = []
+
+def show_image(index):
+    # Carica l'immagine corrispondente all'indice
+    photo = tk.PhotoImage(file=image_array[index])
+    photo_array.append(photo)  # Aggiunge il riferimento all'array
+
+    image_label.config(image=photo)
+
+    if index < len(image_array) - 1:
+        #root.after(750, show_image, index + 1) #questa riga serve per far andare avanti le immagini temporizzate
+        root.bind('<space>', lambda event: show_image(index + 1)) #così le immaigni proseguono solo con la barra spazio
+
+# Mostra la prima immagine
+show_image(0)
+
+root.mainloop()
+
 
 
 
