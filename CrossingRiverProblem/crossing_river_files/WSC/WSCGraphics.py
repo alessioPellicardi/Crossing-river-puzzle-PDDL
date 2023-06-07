5c941a17018a0d896c46f20322956cdcd0c00128
Alessio <a.pellicardi@gmail.com>
2023-06-07 08:34:32 +0200

    fixed WSC version and added new images

--- CrossingRiverProblem/crossing_river_files/WSC/WSCGraphics.py
+++ CrossingRiverProblem/crossing_river_files/WSC/WSCGraphics.py
@@ -12,12 +12,15 @@ print(var_lettura)
 
 numeric_array = []
 image_array = []
+moves_array_temp = []
+moves_array = []
+cont = 0
 mapping = {
-    'trasporta-pecora-destra': 1,
+    'move.sheep-right': 1,
     'empty-going-left': 2,
-    'trasporta-lupo-destra': 3,
-    'trasporta-pecora-sinistra': 4,
-    'trasporta-cavolo-destra': 5
+    'move-wolf-right': 3,
+    'move-sheep-left': 4,
+    'move-cabbage-right': 5
 }
 
 for string in var_lettura:
@@ -25,12 +28,31 @@ for string in var_lettura:
     parts = string.split(' ')
     command = parts[0]
     numeric_array.append(mapping[command])
-    image_name = command + ".png"
-    image_array.append(image_name)
+    moves_array_temp.append(command)
+    # Aggiunta del 2 alla seconda versione delle mosse
+    cont = cont + 1
+    print(cont)
+    if (cont == 5):
+        image_name = command + "-2.png"
+        image_array.append(image_name)
+    elif (cont == 6):
+        image_name = command + "-2.png"
+        image_array.append(image_name)
+    else:
+        image_name = command + ".png"
+        image_array.append(image_name)
 
 print(numeric_array)
+# Inserisco manualmente immagine iniziale e finale
+image_array.insert(0, "startingPoint2Couples.png")
+image_array.insert(len(image_array), "final-situation.png")
+moves_array_temp.insert(0, "starting point")
+moves_array_temp.insert(len(image_array), "finish")
 print(image_array)
 
+for string in moves_array_temp:
+    string = string.strip().replace('-', ' ')
+    moves_array.append(string)
 
 root = tk.Tk()
 
@@ -48,9 +70,13 @@ def show_image(index):
 
     image_label.config(image=photo)
 
-    # Se non siamo all'ultima immagine, aspetta 750ms e mostra la prossima immagine
+    name_label = tk.Label(root, text=moves_array[index], font=("Arial", 25))
+    name_label.grid(row=0, column=0, sticky="nw")  # Posiziona in alto a sinistra
+
     if index < len(image_array) - 1:
-        root.after(750, show_image, index + 1)
+        # root.after(750, show_image, index + 1) #questa riga serve per far andare avanti le immagini temporizzate
+        root.bind('<space>', lambda event: [name_label.destroy(), show_image(
+            index + 1)])  # così le immaigni proseguono solo con la barra spazio
 
 # Mostra la prima immagine
 show_image(0)
